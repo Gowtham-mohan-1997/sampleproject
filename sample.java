f65e3c46a210ba69f1ee3f7499d9acb1ed3c30ef
Gowtham-mohan-1997 <gowthammohan23@gmail.com>
2024-08-28 18:15:12 +0530

    sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,8 @@ public class sample {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("gowtham, mohan!");
+        System.out.println("siva, surya!");
+
 
     }
 }
